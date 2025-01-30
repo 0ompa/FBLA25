8758012db727c0f43b77cd49bfc0ed89beb280bc
0ompa <101219929+0ompa@users.noreply.github.com>
2025-01-30 11:55:14 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          red: {
+            600: "#dc2626",
+            800: "#991b1b",
+          },
+        },
+      },
+    },
+  }
+  
+  
